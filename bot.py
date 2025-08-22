0c6e470c1ed4a19b325427504af9f2849f106d47
Pivertg <gamingpivert@gmail.com>
2025-08-23 00:43:02 +0200

    Add files via upload

--- bot.py
+++ bot.py
@@ -5,24 +5,8 @@ from discord.ext import commands, tasks
 import asyncio
 import json
 from threading import Thread
-from flask import Flask
 import time
 
-# ----- Keep Alive pour Replit -----
-app = Flask('')
-
-@app.route('/')
-def home():
-    return "Bot en ligne !"
-
-def run():
-    app.run(host='0.0.0.0', port=8080)
-
-def keep_alive():
-    t = Thread(target=run)
-    t.start()
-
-keep_alive()
 
 # ---------------------------------
 # ----- Bot Discord -----
